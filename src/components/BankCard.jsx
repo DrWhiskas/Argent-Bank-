01a3134f7b2f272532598d7310ed77c906f0cc7c
unknown <kayv@hotmail.fr>
2023-09-26 10:04:35 +0200

    BankCard
    create card for the bank transaction

---
+++ src/components/BankCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import '../css/bankCard.css'
+import '../css/button.css'
+
+export default function BankCard(){
+    return (
+			<section className="account">
+				<div className="account-content-wrapper">
+					<h3 className="account-title"></h3>
+					<p className="account-amount"></p>
+					<p className="account-amount-description">Available Balance</p>
+				</div>
+				<div className="account-content-wrapper cta">
+					<button className="transaction-button">View transactions</button>
+				</div>
+			</section>
+		);
+}
--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import BankCard from "../components/BankCard";
 
 export default function UserAccount(){
     return(
         <>
             <Header />
+            <BankCard />
             <Footer />
         </>
     )
